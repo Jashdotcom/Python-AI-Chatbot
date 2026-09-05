d17580edf7f1e89bb6535383ca500fab12a792c1
Jash Mistry <jashmistry23@gmail.com>
2026-09-05 17:51:21 +0530

    feat: add interactive AI chatbot with OpenRouter

--- app.py
+++ app.py
@@ -1,8 +1,34 @@
 import os
 from dotenv import load_dotenv
+from openai import OpenAI
 
 load_dotenv()
 
-api_key = os.getenv("sk-370db56b232a19ba-b3323f-f292ea80")
+api_key = os.getenv("OPENROUTER_API_KEY")
 
-print(api_key)
+client = OpenAI(
+    api_key = api_key,
+    base_url="https://openrouter.ai/api/v1"
+)
+
+while True:
+    question = input("\nYou: ")
+
+    if question.lower() == "exit":
+        print("Goodbye!")
+        break
+
+    response = client.chat.completions.create(
+        model="anthropic/claude-sonnet-4.6",
+        messages = [
+            {
+                "role": "user",
+                "content": question
+            }
+        ],
+        max_tokens=500
+    )
+    answer = response.choices[0].message.content
+    print("\nAI: ", answer)
+
+
