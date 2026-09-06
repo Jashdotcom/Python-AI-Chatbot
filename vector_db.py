1671085031fc9ca7de58779751360bf476046835
Jash Mistry <jashmistry23@gmail.com>
2026-09-06 11:09:53 +0530

    feat: create collection & add documents

--- vector_db.py
+++ vector_db.py
@@ -7,3 +7,23 @@ client = chromadb.PersistentClient(
 collection = client.get_or_create_collection(
     name="college_rules"
 )
+
+documents = [
+    "Students must maintain a minimum attendance of 75%.",
+    "The college library is open from 8 AM to 6 PM.",
+    "Students must carry their identity card inside the college."
+]
+
+ids = [
+    "chunk1",
+    "chunk2",
+    "chunk3"
+]
+
+collection.add(
+    documents = documents,
+    ids = ids
+)
+
+print("Documents added successfully!")
+print("Number of documents: ",collection.count())
