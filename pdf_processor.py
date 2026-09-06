8cc25b5a9263e66b0cdd73b2c06f9e985f3f2a4d
Jash Mistry <jashmistry23@gmail.com>
2026-09-06 09:54:59 +0530

    feat: chunking

--- pdf_processor.py
+++ pdf_processor.py
@@ -16,4 +16,19 @@ print("Number of pages: ", len(reader.pages))
 print("\nExtracted text: ")
 print(all_text)
 
+chunk_size = 1000
+
+chunks = []
+
+for i in range(0, len(all_text), chunk_size):
+    chunk = all_text[i:i + chunk_size]
+    chunks.append(chunk)
+
+print("Number of pages: ", len(reader.pages))
+print("Number of chunks: ", len(chunks))
+
+for i, chunk in enumerate(chunks, start=1):
+    print(f"\n---Chunk {i} ---")
+    print(chunk)
+
         
